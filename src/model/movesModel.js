6e9364212d39d34afd6f4d5a3ad30998b68ff495
unknown <FlavioOtt>
2024-08-01 11:28:02 -0300

    movesModel.js | formatPokeMoves | 01/08:11:27

--- src/model/movesModel.js
+++ src/model/movesModel.js
@@ -3,32 +3,59 @@ const axios = require("axios");
 
 async function formatPokeMoves(moves_){
     let moves = [];
-    
+
+    let promises
+
     if (moves_ && moves_.length > 0){
-        moves_.map(move => {
-            console.log(move);
-            moves.push(move)
+
+        promises = moves_.map(async move => {
+
+            let version_group_details = await move.version_group_details.find(i => i.version_group.name == "red-blue");
+
+            if (version_group_details){
+
+                let res = await axios({
+                    method: "get",
+                    url: move.move.url
+                });
+
+                let type_data = await axios({
+                        method: "get",
+                        url: res.data.type.url
+                    })
+           
+                for (infos in res){
+
+                    let attack = res[infos];
+            
+                    if (res[infos].name){
+                        
+                        await moves.push([
+                            {
+                                id: attack.id,
+                                name: attack.name, 
+                                base_damage: attack.power,
+                                type: res.data.type.name,
+                                accuracy: attack.accuracy,
+                                level_learned_at: version_group_details.level_learned_at,
+                                move_learn_method: version_group_details.move_learn_method.name,
+                                damage_relations: type_data.data.damage_relations,
+                                contest_combos: res.contest_combos
+                            }
+                        ]);
+
+                    }
+            
+                }
+    
+            }
         })
 
     }
+    
+    await Promise.all(promises)
 
-    return moves;
+    return await moves;
 }
-
-formatedPokeMoves = {
-    name: "",
-    level: 0,
-    evolutions: [
-        ["de", "para", 0/*lvl necessario*/],
-        ["de", "para", 0/*lvl necessario*/]
-    ],
-    attacks: [
-        {
-            name: "transform", 
-            damage: ""
-        },
-    ]
-}
-
 
 module.exports = { formatPokeMoves }
